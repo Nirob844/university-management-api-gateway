2c7ed258ec1936f3f15bd0b937e4ff2ab794654a
Nirob Hasan <nirob427417@gmail.com>
2023-09-14 20:04:24 +0600

    redis

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 //app.use('/api/v1', routes);
 app.get('/', (req, res) => {
-  res.send('Server started Successfully');
+  res.send('Server started Successfully ...');
 });
 app.use(globalExceptionHandler);
 
