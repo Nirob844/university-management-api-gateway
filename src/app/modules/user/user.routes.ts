f48c4b2c3f4ccb56de038181f172182df42c2fa7
Nirob Hasan <nirob427417@gmail.com>
2023-09-21 14:55:37 +0600

    create faculty

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -17,15 +17,15 @@ router.post(
   }
 );
 
-// router.post(
-//     '/create-faculty',
-//     auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//     FileUploadHelper.upload.single('file'),
-//     (req: Request, res: Response, next: NextFunction) => {
-//         req.body = UserValidation.createFaculty.parse(JSON.parse(req.body.data));
-//         return UserController.createFaculty(req, res, next);
-//     }
-// );
+router.post(
+  '/create-faculty',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FileUploadHelper.upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = UserValidation.createFaculty.parse(JSON.parse(req.body.data));
+    return UserController.createFaculty(req, res, next);
+  }
+);
 
 // router.post(
 //     '/create-admin',
